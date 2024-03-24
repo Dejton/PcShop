cc00e348628277e792f83e34787c300ec601b6cf
Dejton <patryk.firlus@gmail.com>
2024-03-24 09:49:08 +0100

    test

--- src/main/java/com/springsecurity/pcshop/Peripherals/Entity/HardDisc.java
+++ src/main/java/com/springsecurity/pcshop/Peripherals/Entity/HardDisc.java
@@ -11,6 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+//klasa Patryka
 public class HardDisc extends Product {
     @Column(name = "memory_type", nullable = false)
     private String memoryType;
