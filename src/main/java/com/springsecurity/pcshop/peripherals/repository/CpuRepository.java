486b74d9b6a6cc9e1f4501c714fe29aed6f7b11c
Dejton <patryk.firlus@gmail.com>
2024-03-24 10:31:54 +0100

    Refactoring

--- src/main/java/com/springsecurity/pcshop/peripherals/repository/CpuRepository.java
+++
@@ -1,8 +0,0 @@
-package com.springsecurity.pcshop.peripherals.repository;
-
-import com.springsecurity.pcshop.peripherals.entity.Cpu;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CpuRepository extends JpaRepository<Cpu,Long> {
-
-}
